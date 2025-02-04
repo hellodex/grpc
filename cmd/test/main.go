6c41d8553345fa8983120e8b8a1534c6dc21ff69
line <line@s.com>
2025-02-04 08:04:08 +0000

    1

---
+++ cmd/test/main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"context"
+	"crypto/x509"
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	pb "github.com/rpcpool/yellowstone-grpc/examples/golang/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/metadata"
+)
+
+var token = ""
+var address = "solana-yellowstone-grpc.publicnode.com:443"
+
+func main() {
+	log.SetOutput(os.Stdout)
+	conn := grpc_connect(address, false)
+	defer conn.Close()
+	go stdERR()
+	grpc_subscribe(conn)
+}
+
+var kacp = keepalive.ClientParameters{
+	Time:                10 * time.Second,
+	Timeout:             time.Second,
+	PermitWithoutStream: true,
+}
+
+func grpc_connect(address string, plaintext bool) *grpc.ClientConn {
+	var opts []grpc.DialOption
+	if plaintext {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	} else {
+		pool, _ := x509.SystemCertPool()
+		creds := credentials.NewClientTLSFromCert(pool, "")
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+
+	opts = append(opts, grpc.WithKeepaliveParams(kacp))
+
+	log.Println("Starting grpc client, connecting to", address)
+	conn, err := grpc.NewClient(address, opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+
+	return conn
+}
+
+func ptr[T any](v T) *T {
+	return &v
+}
+
+func grpc_subscribe(conn *grpc.ClientConn) {
+	var err error
+	client := pb.NewGeyserClient(conn)
+
+	subscription := pb.SubscribeRequest{
+		Transactions: map[string]*pb.SubscribeRequestFilterTransactions{
+			"txn": {
+				Vote:            ptr(false),
+				Failed:          ptr(false),
+				Signature:       nil,
+				AccountInclude:  []string{},
+				AccountExclude:  []string{},
+				AccountRequired: []string{},
+			},
+		},
+		Commitment: pb.CommitmentLevel_FINALIZED.Enum(),
+		Ping:       nil,
+	}
+
+	ctx := context.Background()
+	if token != "" {
+		md := metadata.New(map[string]string{"x-token": token})
+		ctx = metadata.NewOutgoingContext(ctx, md)
+	}
+
+	stream, err := client.Subscribe(ctx)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	err = stream.Send(&subscription)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	var i uint = 0
+	var lastSlot uint64
+	var lastTimestamp int64
+	var initialized bool
+
+	for {
+		i += 1
+		resp, err := stream.Recv()
+		currentTimestamp := time.Now().UnixMilli()
+
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatalf("Error occurred in receiving update: %v", err)
+		}
+
+		tx := resp.GetTransaction()
+		if tx == nil {
+			log.Println("Received non-transaction message, continuing...")
+			log.Printf("CurrentTimestamp: %v %v", currentTimestamp, resp)
+			continue
+		}
+
+		currentSlot := tx.GetSlot()
+
+		if !initialized {
+			lastSlot = currentSlot
+			lastTimestamp = currentTimestamp
+			initialized = true
+			log.Printf("Initial slot: %v at Timestamp: %v", lastSlot, lastTimestamp)
+			continue
+		}
+
+		if currentSlot > lastSlot {
+			timeDiff := currentTimestamp - lastTimestamp
+			log.Printf("New slot: %v, Previous slot: %v, CurrentTimestamp: %v",
+				currentSlot,
+				lastSlot,
+				currentTimestamp)
+			log.Printf("Time gap since last : %d ms", timeDiff)
+
+			lastSlot = currentSlot
+			lastTimestamp = currentTimestamp
+		}
+
+		if data, err := json.Marshal(tx); err == nil {
+			msg <- data
+		}
+
+	}
+}
+
+var msg = make(chan []byte, 1024)
+
+func stdERR() {
+	for d := range msg {
+		os.Stderr.WriteString(string(d) + "\n")
+	}
+}
